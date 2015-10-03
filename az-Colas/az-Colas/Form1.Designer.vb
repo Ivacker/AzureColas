7847e32da370202ce13c51ef6d06029a4b4e4993
Ivacker <ivacker@outlook.com>
2015-10-03 22:32:27 +0000

    primer check

---
+++ az-Colas/az-Colas/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnCrearCola = New System.Windows.Forms.Button()
+        Me.btnCrearMensajeCola = New System.Windows.Forms.Button()
+        Me.btnLeerMensajeCola = New System.Windows.Forms.Button()
+        Me.btnEliminarMensaje = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnCrearCola
+        '
+        Me.btnCrearCola.Location = New System.Drawing.Point(12, 23)
+        Me.btnCrearCola.Name = "btnCrearCola"
+        Me.btnCrearCola.Size = New System.Drawing.Size(156, 25)
+        Me.btnCrearCola.TabIndex = 0
+        Me.btnCrearCola.Text = "Crear Cola"
+        Me.btnCrearCola.UseVisualStyleBackColor = True
+        '
+        'btnCrearMensajeCola
+        '
+        Me.btnCrearMensajeCola.Location = New System.Drawing.Point(12, 54)
+        Me.btnCrearMensajeCola.Name = "btnCrearMensajeCola"
+        Me.btnCrearMensajeCola.Size = New System.Drawing.Size(156, 25)
+        Me.btnCrearMensajeCola.TabIndex = 1
+        Me.btnCrearMensajeCola.Text = "Crear Mensaje en Cola"
+        Me.btnCrearMensajeCola.UseVisualStyleBackColor = True
+        '
+        'btnLeerMensajeCola
+        '
+        Me.btnLeerMensajeCola.Location = New System.Drawing.Point(12, 85)
+        Me.btnLeerMensajeCola.Name = "btnLeerMensajeCola"
+        Me.btnLeerMensajeCola.Size = New System.Drawing.Size(156, 25)
+        Me.btnLeerMensajeCola.TabIndex = 3
+        Me.btnLeerMensajeCola.Text = "Leer Mensaje en Cola"
+        Me.btnLeerMensajeCola.UseVisualStyleBackColor = True
+        '
+        'btnEliminarMensaje
+        '
+        Me.btnEliminarMensaje.Location = New System.Drawing.Point(12, 116)
+        Me.btnEliminarMensaje.Name = "btnEliminarMensaje"
+        Me.btnEliminarMensaje.Size = New System.Drawing.Size(156, 25)
+        Me.btnEliminarMensaje.TabIndex = 4
+        Me.btnEliminarMensaje.Text = "Eliminar Mensaje en Cola"
+        Me.btnEliminarMensaje.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(441, 301)
+        Me.Controls.Add(Me.btnEliminarMensaje)
+        Me.Controls.Add(Me.btnLeerMensajeCola)
+        Me.Controls.Add(Me.btnCrearMensajeCola)
+        Me.Controls.Add(Me.btnCrearCola)
+        Me.Font = New System.Drawing.Font("Calibri", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "@ivacker - Uso de colas con Azure Storage"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnCrearCola As Button
+    Friend WithEvents btnCrearMensajeCola As Button
+    Friend WithEvents btnLeerMensajeCola As Button
+    Friend WithEvents btnEliminarMensaje As Button
+End Class
---
+++ az-Colas/az-Colas/Form1.vb
@@ -0,0 +1,106 @@
+﻿Imports Microsoft.WindowsAzure.Storage
+Imports Microsoft.WindowsAzure.Storage.Auth
+Imports Microsoft.WindowsAzure.Storage.Queue
+
+Public Class Form1
+
+    'Declaraciones
+    Private m_csa_storageAccount As CloudStorageAccount
+    Dim sNombreCola As String = Nothing
+
+    Public ReadOnly Property Csa_storageAccount() As CloudStorageAccount
+        Get
+            If m_csa_storageAccount Is Nothing Then
+                Dim strAccount As String = My.Settings.sCuenta
+                Dim strKey As String = My.Settings.sKey
+
+                Dim credential As New StorageCredentials(strAccount, strKey)
+                m_csa_storageAccount = New CloudStorageAccount(credential, True)
+            End If
+            Return m_csa_storageAccount
+        End Get
+
+    End Property
+
+    Sub CREARCola()
+
+        Dim cClient As CloudQueueClient = Csa_storageAccount.CreateCloudQueueClient 'Creo el objeto cliente
+        Dim Queue = cClient.GetQueueReference(My.Settings.sNombreCola.ToString) 'Obtengo el objeto cola
+        Queue.CreateIfNotExists() ' Creo la cola si no existe
+        MsgBox("Cola creada: " & sNombreCola.ToString)
+
+    End Sub
+    Sub PUTCola(ByVal sMensaje As String)
+
+        Dim cClient As CloudQueueClient = Csa_storageAccount.CreateCloudQueueClient 'Creo el objeto cliente
+        Dim Queue = cClient.GetQueueReference(My.Settings.sNombreCola.ToString) 'Obtengo el objeto cola
+
+        Dim Message As New CloudQueueMessage(sMensaje) 'Creo el objeto mensaje
+        Queue.AddMessage(Message) ' Coloco el mensaje en la cola
+
+    End Sub
+
+    Sub GETCola()
+
+        Dim cClient As CloudQueueClient = Csa_storageAccount.CreateCloudQueueClient 'Creo el objeto cliente
+        Dim Queue = cClient.GetQueueReference(My.Settings.sNombreCola.ToString) 'Obtengo el objeto cola
+
+        ' obtener mensaje
+        Dim retrievedMessage = Queue.GetMessage 'Obtengo el mensaje desde la cola
+
+        Try
+            Queue.GetMessage(TimeSpan.FromSeconds(5)) 'Obtengo el mensaje y lo retengo por 5 segundos
+            MsgBox("Mensaje obtenido de la cola: " & retrievedMessage.AsString) 'Visualizo el contenido del mensaje de la cola
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Sub ELIMINACola()
+
+        Dim cClient As CloudQueueClient = Csa_storageAccount.CreateCloudQueueClient 'Creo el objeto cliente
+        Dim Queue = cClient.GetQueueReference(My.Settings.sNombreCola.ToString) 'Obtengo el objeto cola
+
+        ' obtener mensaje
+        Dim retrievedMessage = Queue.GetMessage 'Obtengo el mensaje desde la cola
+
+        Try
+            Queue.DeleteMessage(retrievedMessage.Id.ToString, retrievedMessage.PopReceipt) 'Elimino el mensaje desde la cola indicando el ID
+            MsgBox("Mensaje borrado de la cola :" & retrievedMessage.Id.ToString) 'Visualizo el ID del mensaje eliminado
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnCrearCola.Click
+
+        sNombreCola = InputBox("Escriba el nombre para la Cola:", "Crear Cola de Mensajes")
+
+        If sNombreCola.Length > 0 Then
+            My.Settings.sNombreCola = sNombreCola.ToString
+            My.Settings.Save()
+            CREARCola()
+        Else
+            MsgBox("Debe ingresar un nombre para la cola, no se creo la cola",, "Aviso")
+        End If
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles btnCrearMensajeCola.Click
+
+        PUTCola(InputBox("Escriba el mensaje para la cola:", "Crear Mensaje en cola"))
+        MsgBox("Mensaje Creado")
+
+    End Sub
+
+    Private Sub btnLeerMensajeCola_Click(sender As Object, e As EventArgs) Handles btnLeerMensajeCola.Click
+        GETCola()
+    End Sub
+
+    Private Sub btnEliminarMensaje_Click(sender As Object, e As EventArgs) Handles btnEliminarMensaje.Click
+        ELIMINACola()
+    End Sub
+End Class
